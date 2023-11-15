a68bd9ab59fe763d5d1c2aad47859227c7e3dfe5
Santiago Ambricca <117587525+SAmbricca@users.noreply.github.com>
2023-11-15 11:13:09 -0300

    Create objetivo.py

---
+++ Codigo/objetivo.py
@@ -0,0 +1,18 @@
+import pygame
+from constantes import *
+from cajas import *
+from paredes import *
+
+class Objetivo:
+        
+    def __init__(self, x, y):
+            self.image = pygame.image.load("imagenes/dock.png")
+            self.image = pygame.transform.scale(self.image, (70,70))
+            self.rect = self.image.get_rect()
+            self.rect.x = x
+            self.rect.y = y
+
+            self.rect_objetivo = pygame.Rect(self.rect.x, self.rect.y, 70,70)
+
+    def dibujar(self, pantalla):
+        pantalla.blit(self.image, self.rect)
